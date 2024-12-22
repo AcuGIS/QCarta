b9255a04fb9265381704c11e5ae143f2c76a5c13
David Ghedini <david@davidghedini.com>
2024-12-22 14:20:38 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ pygments_style = "sphinx"
 html_logo = "quail-logo-docs.png"
 html_theme_options = {
     'logo_only': True,
-    'prev_next_buttons_location': both,
+    'prev_next_buttons_location': 'both',
     'display_version': False,
     'navigation_depth': -1    
 }
