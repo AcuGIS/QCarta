1921de505399f038f43281704214de9dab8a4ea6
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2025-02-01 22:12:53 +0200

    fix leaflet.draw icon path

--- assets/dist/js/L.BetterWMS.js
+++ assets/dist/js/L.BetterWMS.js
@@ -88,7 +88,7 @@ L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
   
   showGetFeatureInfo: function (err, latlng, content) {
     if (err) { console.log(err); return; } // do nothing if there's an error
-    
+
     // Otherwise show the content in a popup, or something.
     L.popup({ maxWidth: 800})
       .setLatLng(latlng)
