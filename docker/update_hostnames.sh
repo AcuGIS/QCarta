eb2d8f8f43c747ec9afe2b62258b104932c5c0fc
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2025-02-02 14:49:53 +0200

    docker: update str_replace pattern

--- docker/update_hostnames.sh
+++ docker/update_hostnames.sh
@@ -5,11 +5,11 @@ sed -i.save 's/localhost/db/' /var/www/html/admin/action/pglink.php
 sed -i.save 's/localhost/db/' /var/www/html/admin/action/import.php
 sed -i.save 's/localhost/web/' /var/www/html/admin/class/mapproxy.php
 sed -i.save "s|'\.\$_SERVER\['HTTP_HOST'\]\.'|localhost|" /var/www/html/admin/snippets/wms_index.php
-sed -i.save "s|str_replace('://:0|str_replace('://localhost|" /var/www/html/admin/snippets/qgs_svc.php
+sed -i.save "s|str_replace('http://localhost|str_replace('http://:0|" /var/www/html/admin/snippets/qgs_svc.php
 
 for i in 2 3 4 5 6 7 8; do
     for s in wfs wms wmts; do
-        sed -i.save "s|str_replace('://:0|str_replace('://localhost|" /var/www/html/stores/${i}/${s}.php
+        sed -i.save "s|str_replace('http://localhost|str_replace('http://:0|" /var/www/html/stores/${i}/${s}.php
     done
 done
 
