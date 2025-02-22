7f861738571ad6f8f6178fe84b5d21cb18e6de83
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2025-02-22 20:51:19 +0200

    update store wfs/wms/wmts.php

--- installer/demo/html/stores/2/wmts.php
+++ installer/demo/html/stores/2/wmts.php
@@ -1,29 +1,36 @@
 <?php
-	//const QGIS_FILENAME = 'QGIS_FILE_VALUE';
 	include('../../admin/incl/index_prefix.php');
+	include('../../admin/incl/qgis.php');
 
-	$format = '';
-	if(preg_match('/FORMAT=([a-z]+)&/',$_SERVER['QUERY_STRING'], $matches)){
-		$format = $matches[1];
-		if(strcasecmp($format, 'pdf') == 0){
-			header("Content-Type: application/pdf");
-			header('Content-Disposition: attachment; filename="'.str_replace('.qgs', '.pdf', basename(QGIS_FILENAME)).'"');
-		}else if(strcasecmp($format, 'png') == 0){
-			header("Content-Type: application/png");
-			header('Content-Disposition: attachment; filename="'.str_replace('.qgs', '.png', basename(QGIS_FILENAME)).'"');
-		}else{
-			header("Content-Type: text/xml");
-		}
+	$_GET = array_change_key_case($_GET, CASE_LOWER); 
+	
+	$format = null;
+	if(($_GET['request'] == 'GetFeature') && (!empty($_GET['outputformat'])) ){
+		$format = urldecode($_GET['outputformat']);
+	}else if(($_GET['request'] == 'GetFeatureInfo') && (!empty($_GET['info_format'])) ){
+		$format = urldecode($_GET['info_format']);
+	}else if($_GET['request'] == 'GetProjectSettings') {
+		$format = 'text/xml';
+	}else if($_GET['request'] == 'XSL') {
+		$format = 'text/xml';
+	}else if($_GET['request'] == 'GetCapabilities') {
+		$format = 'text/xml';
+	}else if(!empty($_GET['format'])){
+		$format = urldecode($_GET['format']);
 	}else{
-		header("Content-Type: text/xml");
+		http_response_code(400);	// Bad Request
+		die(400);
 	}
+
+	format2headers($format, LAYER_ID);
 	
 	$url = 'http://localhost/cgi-bin/qgis_mapserv.fcgi?VERSION=1.1.0&map='.QGIS_FILENAME_ENCODED.'&SERVICE=WMTS&'.$_SERVER['QUERY_STRING'];
 	if($_GET['request'] == 'GetCapabilities') {
 	   $content = file_get_contents($url);
 	   $content = preg_replace('/MAP=[^&"]*(&amp;)?/i', '', $content);
 	   $acc_key = isset($_GET['access_key']) ? 'access_key='.$_GET['access_key'].'&amp;' : '';
-       echo str_replace('://localhost/cgi-bin/qgis_mapserv.fcgi?', '://'.$_SERVER['HTTP_HOST'].'/stores/'.LAYER_ID.'/'.strtolower('WMTS').'.php?'.$acc_key, $content);
+	   $out_proto = empty($_SERVER['HTTPS']) ? 'http' : 'https';
+       echo str_replace('http://localhost/cgi-bin/qgis_mapserv.fcgi?', $out_proto.'://'.$_SERVER['HTTP_HOST'].'/stores/'.LAYER_ID.'/'.strtolower('WMTS').'.php?'.$acc_key, $content);
 	}else{
 	   readfile($url);
 	}
