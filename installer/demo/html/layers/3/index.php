eb8cae4841797ee3b55b66ee488b11118658d304
Kaloyan Petrov <kaloyan_petrov@mail.bg>
2025-04-10 17:38:46 +0300

    include JQ on maps without features

--- installer/demo/html/layers/3/index.php
+++ installer/demo/html/layers/3/index.php
@@ -63,12 +63,14 @@
 	<script src="../../assets/dist/js/L.Control.Opacity.js"></script>
 	<link rel="stylesheet" href="../../assets/dist/css/wms_index.css"/>
 	
+	<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+	
 	<?php if(count($feature_names)){ ?>
-	<link href="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.2.2/b-3.2.2/b-html5-3.2.2/datatables.min.css" rel="stylesheet" integrity="sha384-8/teZDJvKonhCW0gzEq7h+7isOuQtsttozTAZnCt86gaZKLAMET4OfRS09qYO8wO" crossorigin="anonymous">
+	<link href="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.2.2/b-3.2.2/b-html5-3.2.2/datatables.min.css" rel="stylesheet" integrity="sha384-8/teZDJvKonhCW0gzEq7h+7isOuQtsttozTAZnCt86gaZKLAMET4OfRS09qYO8wO" crossorigin="anonymous">
 
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js" integrity="sha384-VFQrHzqBh5qiJIU0uGU5CIW3+OWpdGGJM9LBnGbuIH2mkICcFZ7lPd/AAtI7SNf7" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js" integrity="sha384-/RlQG9uf0M2vcTw3CX7fbqgbj/h8wKxw7C3zu9/GxcBPRKOEcESxaxufwRXqzq6n" crossorigin="anonymous"></script>
-    <script src="https://cdn.datatables.net/v/bs5/jq-3.7.0/jszip-3.10.1/dt-2.2.2/b-3.2.2/b-html5-3.2.2/datatables.min.js" integrity="sha384-G21/IAOAMg4/9nYB3ZZGTQFatV1Z0pPjQMCiFDfZybF+BJ1wL/SgwqUWBWYNWfxE" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/v/bs5/jszip-3.10.1/dt-2.2.2/b-3.2.2/b-html5-3.2.2/datatables.min.js" integrity="sha384-G21/IAOAMg4/9nYB3ZZGTQFatV1Z0pPjQMCiFDfZybF+BJ1wL/SgwqUWBWYNWfxE" crossorigin="anonymous"></script>
  
 	<?php } ?>
 </head>
