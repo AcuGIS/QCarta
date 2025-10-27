f4acc98ec6613cfc00b41d1353339a414ad158d1
David Ghedini <david@davidghedini.com>
2025-10-28 01:38:55 +0200

    Update login.php

--- login.php
+++ login.php
@@ -11,7 +11,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>QCarta</title>
+<title>QCarta Login</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <script src="https://cdn.tailwindcss.com"></script>
